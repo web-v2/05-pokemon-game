2ad5390a9c4ae7aa2e4a4c73295bc32d779af277
Samir Vergara V <svergara3@misena.edu.co>
2022-07-02 02:20:33 -0500

    Avance

--- src/helpers/getPokemonOptions.js
+++ src/helpers/getPokemonOptions.js
@@ -7,25 +7,23 @@ const getPokemon = () =>{
 const getPokemonOptions = async() =>{
     const mixedPokemons = getPokemon().sort( ()=> Math.random() -0.5 )
     const pokemons = await getPokemonNames( mixedPokemons.splice(0,4) );
-    //console.table(pokemons);
+    console.table(pokemons);
     return pokemons;
 }
 
-const getPokemonNames = async([a,b,c,d] = []) =>{
-    const arrProm = [
-        pokemonAPI.get(`/${a}`),
-        pokemonAPI.get(`/${b}`),
-        pokemonAPI.get(`/${c}`),
-        pokemonAPI.get(`/${d}`)
-    ];
+const getPokemonNames = async(pokemons = []) =>{
+    const promesas = pokemons.map((id) => {
+      return pokemonAPI.get(`/${id}`);
+    }); 
 
-    const [p1, p2, p3, p4] = await Promise.all(arrProm);
-    return [
-        {name: p1.data.name, id: p1.data.id},
-        {name: p2.data.name, id: p2.data.id},
-        {name: p3.data.name, id: p3.data.id},
-        {name: p4.data.name, id: p4.data.id}
-    ]
+    const respuestas = await Promise.all(promesas);
+    const retorno = respuestas.map((rta) => {
+      return {
+        name: rta.data.name,
+        id: rta.data.id,
+      };
+    });
+    return retorno;  
 }
 
 export default getPokemonOptions;
