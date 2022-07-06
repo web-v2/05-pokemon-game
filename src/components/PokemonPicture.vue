a90fbe77ad401e4783e04714337de410a44bc3a9
Samir Vergara V <svergara3@misena.edu.co>
2022-07-05 20:04:44 -0500

    Listo para Produccion

--- src/components/PokemonPicture.vue
+++ src/components/PokemonPicture.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="pokemon-container">
-    <img :src="imgSrc" 
+    <img v-if="!showPokemon" 
+         :src="imgSrc" 
          class="hidden-pokemon"
          alt="pokemon">
-    <img v-if="showPokemon" 
+    <img v-else 
          :src="imgSrc" 
          class="fade-in"
          alt="pokemon">         
--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -4,11 +4,14 @@
     <div v-else>
       <h1>¿Quién es este pokémon?</h1>
       <PokemonPictureVue :pokemonId="pokemon.id" :showPokemon="showPokemon"/>
-      <PokemonOptionsVue :pokemons="pokemonArr" @selection="checkAnswer"/>    
+      <PokemonOptionsVue :pokemons="pokemonArr" @selectionPokemon="checkAnswer"/>    
+
       <template v-if="showAnswer">
-        <h2 class="fade-in">{{ message }}</h2>
-        <button @click="newGame">Nuevo Juego</button>
+        <h2 v-if="verify" class="fade-in resp-tru">{{ message }}</h2>
+        <h2 v-else class="fade-in resp-false">{{ message }}</h2>
+        <button @click="newGame" class="btn-newGame">Nuevo Juego</button>
       </template>
+      <h3>Desarrollado por Samir V2.</h3>
     </div>
   </div>
 </template>
@@ -16,9 +19,8 @@
 <script>
 import PokemonPictureVue from '@/components/PokemonPicture.vue'
 import PokemonOptionsVue from '@/components/PokemonOptions.vue'
-
 import getPokemonOptions from '@/helpers/getPokemonOptions'
-//console.log(getPokemonOptions());
+
 
 export default {
   components: { PokemonPictureVue, PokemonOptionsVue },
@@ -28,7 +30,8 @@ export default {
         pokemon:      null,
         showPokemon:  false,
         showAnswer:   false,
-        message:      ''
+        message:      '',
+        verify:       false
     }
   },
   methods: { 
@@ -42,22 +45,47 @@ export default {
       this.showAnswer = true;
 
       if(id === this.pokemon.id){
+        this.verify = true
         this.message = `Correcto, es ${this.pokemon.name}`
       }else{
+        this.verify = false
         this.message = `Oops, era ${this.pokemon.name}`
       }
 
     },
     newGame(){
-      this.showPokemon = false;
-      this.showAnswer = false;
-      this.pokemonArr = [];
+      this.showPokemon  = false
+      this.showAnswer   = false
+      this.pokemonArr   = []
+      this.pokemon      = null
       this.mixPokemonsArray()
     }
   },
   mounted(){
     this.mixPokemonsArray()
-    //console.log('Mounted');
   }
 }
 </script>
+
+<style scoped>
+.btn-newGame {
+  background-color: #4CAF50; /* Green */
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  border-radius: 8px;
+}
+.resp-tru{
+  color:#3d9640;
+  text-align: center;
+  font-weight: bold;
+}
+.resp-false{
+  color:#e91644;
+  text-align: center;
+}
+</style>
